254ffcdf055cc47000ec7665d68e797b7cd3d325
William Roberson <billkeyshawn03@gmail.com>
2023-05-08 12:09:01 -0400

    objectnames

---
+++ exercises/ObjectScripts/label_maker.js
@@ -0,0 +1,18 @@
+let myInfo = {
+    name: "Cash Outt", 
+    address: "251 Get bread ave",
+    city: "Bedsty",
+    state: "NY",
+    zip: "18994",
+   };
+
+function printContact(myInfo) {
+    console.log(myInfo.name);
+    console.log(myInfo.address);
+    console.log(myInfo.city);
+    console.log(myInfo.state);
+    console.log(myInfo.zip);
+}   
+  printContact(myInfo); 
+
+   
