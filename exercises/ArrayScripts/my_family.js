e9bd3fc96514f237527021f0a70d42d4970ed5bf
William Roberson <billkeyshawn03@gmail.com>
2023-05-09 13:06:20 -0400

    family

---
+++ exercises/ArrayScripts/my_family.js
@@ -0,0 +1,14 @@
+let family = ["LaKeisha", "Erie", "Chunks","Erie'onna","Bill"];
+
+// let index = 0;
+//  while (index < family.length) {
+    // console.log(family[index]);
+    //  let familyMemeberName = family[index];
+    //  console.log(family.length);
+    // index++;
+//  }
+for (let index = 0; index < family.length; index++) {
+   let familyMemeberName = family[index];
+   console.log(familyMemeberName);
+  }
+ 
