79761ada5eeeaa2be152c4e6c388172ab96778b8
William Roberson <billkeyshawn03@gmail.com>
2023-05-10 16:02:39 -0400

    examples

--- examples/2-28_sorting_array_numbers.js
+++ examples/2-28_sorting_array_numbers.js
@@ -1,3 +1,24 @@
+function compareAscendingNumber(a, b) {
+    if (a < b) return -1; // right order
+    else if (a == b) return 0; // same values
+    else return 1; // swap, wrong order
+   }
+
 let numbers = [3, 27, 400, 1, 111, 5];
-numbers.sort(); 
+numbers.sort(compareAscendingNumber); 
 console.log(numbers);
+
+
+console.log("---------------");
+
+function compareAscendingNumber(a, b) {
+    if (a < b) return -1; // right order
+    else if (a == b) return 0; // same values
+    else return 1; // swap, wrong order
+   }
+
+   numbers.sort(compareAscendingNumber);
+   console.log(numbers);
+
+
+
